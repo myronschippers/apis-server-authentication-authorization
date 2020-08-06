3e7b916ce5b581265c0c11312e0cd0e81b6468f7
Myron <dubtwee@gmail.com>
2020-08-06 10:26:56 -0500

    adding route to get annual reviews

--- server/routes/annualReviews.router.js
+++ server/routes/annualReviews.router.js
@@ -3,7 +3,19 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send([]);
+  // because of passport
+  // req.user
+  const queryText = `SELECT annual_reviews.*, user.username, user.id as user_id FROM "annual_reviews"
+    JOIN "user" ON annual_reviews.employee_id = user.id;`
+
+  pool.query(queryText)
+    .then((dbResp) => {
+      res.send(dbResp.rows);
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(500);
+    })
 });
 
 module.exports = router;
