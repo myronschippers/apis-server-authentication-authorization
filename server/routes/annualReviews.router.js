51f3e61168ad23809ba9b516edae539cfaa16428
Myron <dubtwee@gmail.com>
2020-08-06 11:11:23 -0500

    adding comments for passport

--- server/routes/annualReviews.router.js
+++ server/routes/annualReviews.router.js
@@ -6,17 +6,21 @@ const router = express.Router();
 router.get('/', rejectUnauthenticated, (req, res) => {
   // because of passport
   // req.user
+  // req.isAuthenticated()
   const queryText = `SELECT "annual_review".*, "user".username, "user".id as user_id FROM "annual_review"
-    JOIN "user" ON "annual_review".employee_id = "user".id;`
+    JOIN "user" ON "annual_review".employee_id = "user".id
+    WHERE "annual_review".access_level < $1;`
 
-  pool.query(queryText)
+  pool.query(queryText, [
+    req.user.access_level
+  ])
     .then((dbResp) => {
       res.send(dbResp.rows);
     })
     .catch((err) => {
       console.log(err)
       res.sendStatus(500);
-    })
+    });
 });
 
 module.exports = router;
